5ccce8d5c0384b80e40cd4441ae9c88d3a52497c
sj <1090534619@qq/com>
2019-03-13 21:58:57 +0800

    程序测试

--- app/src/main/java/org/sj/testdemo/zxing/AnyOrientationCaptureActivity.java
+++ app/src/main/java/org/sj/testdemo/zxing/AnyOrientationCaptureActivity.java
@@ -5,6 +5,7 @@ import com.journeyapps.barcodescanner.CaptureActivity;
 /**
  * This Activity is exactly the same as CaptureActivity, but has a different orientation
  * setting in AndroidManifest.xml.
+ * 很纠结
  */
 public class AnyOrientationCaptureActivity extends CaptureActivity {
 
